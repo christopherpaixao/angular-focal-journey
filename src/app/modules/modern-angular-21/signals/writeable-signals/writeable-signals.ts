61cc81804dd67e1f7c10cd6d9e2c4db1e2d1d9b8
christopherpaixao <cpaixaoneto@gmail.com>
2026-07-28 09:13:53 -0300

    modificando writeables signals
    atualiza my-signals para simular os metodos set e update do signal

--- src/app/modules/modern-angular-21/signals/writeable-signals/writeable-signals.ts
+++ src/app/modules/modern-angular-21/signals/writeable-signals/writeable-signals.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 import { mySignal } from './my-signal';
 
 @Component({
@@ -6,16 +6,27 @@ import { mySignal } from './my-signal';
   imports: [],
   templateUrl: './writeable-signals.html',
   styleUrl: './writeable-signals.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WriteableSignals {
-  // readonly fristSignals = signal(42);
-  // readonly secondSignals = signal('Signals');
+  // readonly firstSignal = mySignal(52);
+  // readonly secondSignal = mySignal('MySignals');
 
-  readonly fristSignals = mySignal(52);
-  readonly secondSignals = mySignal('MySignals');
+  readonly firstSignal = signal(42);
+  readonly secondSignal = signal('Signals');
+
+  //Define um novo valor
+  setSignal() {
+    this.firstSignal.set(10);
+  }
+
+  //define um novo valor que depende do valor original (Não recebe um valor, recebe uma função que calcula o valor)
+  updateSignal() {
+    this.firstSignal.update((value) => value + 1);
+  }
 
   constructor() {
-    console.log('The firstSignals value is:', this.fristSignals());
-    console.log('The secondSignals value is:', this.secondSignals());
+    console.log('The firstSignals value is:', this.firstSignal());
+    console.log('The secondSignals value is:', this.secondSignal());
   }
 }
